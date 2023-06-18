750e5770e6108e028249554b94717e86f7ecfe78
Adrian Debowski <adrian.debowski@outlook.com>
2023-06-18 10:25:47 +0200

    Created model class for task.

---
+++ todo/src/main/java/com/aivid/todo/model/Task.java
@@ -0,0 +1,94 @@
+package com.aivid.todo.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Task {
+
+    private UUID uuid;
+    private String taskName;
+    private String taskDescription;
+    private String category;
+    private String priority;
+    private LocalDateTime taskStartTime;
+    private LocalDateTime taskEndTime;
+    private LocalDateTime taskDueTime;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public LocalDateTime getTaskStartTime() {
+        return taskStartTime;
+    }
+
+    public void setTaskStartTime(LocalDateTime taskStartTime) {
+        this.taskStartTime = taskStartTime;
+    }
+
+    public LocalDateTime getTaskEndTime() {
+        return taskEndTime;
+    }
+
+    public void setTaskEndTime(LocalDateTime taskEndTime) {
+        this.taskEndTime = taskEndTime;
+    }
+
+    public LocalDateTime getTaskDueTime() {
+        return taskDueTime;
+    }
+
+    public void setTaskDueTime(LocalDateTime taskDueTime) {
+        this.taskDueTime = taskDueTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(uuid, task.uuid) && Objects.equals(taskName, task.taskName) && Objects.equals(taskDescription, task.taskDescription) && Objects.equals(category, task.category) && Objects.equals(priority, task.priority) && Objects.equals(taskStartTime, task.taskStartTime) && Objects.equals(taskEndTime, task.taskEndTime) && Objects.equals(taskDueTime, task.taskDueTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid, taskName, taskDescription, category, priority, taskStartTime, taskEndTime, taskDueTime);
+    }
+}
