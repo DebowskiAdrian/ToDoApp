deef7860ab09bbb9ea7b000053a3e0b8d8181273
Adrian Debowski <adrian.debowski@outlook.com>
2023-06-19 20:04:41 +0200

    Created TaskService class and test class for it.

---
+++ todo/src/main/java/com/aivid/todo/service/TaskService.java
@@ -0,0 +1,18 @@
+package com.aivid.todo.service;
+
+import com.aivid.todo.model.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class TaskService {
+
+    private List<Task> taskList = new ArrayList<Task>();
+
+    public void addTask(Task task){
+
+    }
+
+
+}
---
+++ todo/src/test/java/com/aivid/todo/service/TaskServiceTest.java
@@ -0,0 +1,19 @@
+package com.aivid.todo.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceTest {
+
+
+
+
+    @Test
+    @DisplayName("Test which verify if task is added correctly")
+    void addTask() {
+    }
+}
