b131120e84f71466cdccfe6dac526df198bd1c63
Adrian Debowski <adrian.debowski@outlook.com>
2023-06-19 20:07:07 +0200

    Added basinc crud operation to TaskRepository interface.

--- todo/src/main/java/com/aivid/todo/repository/TaskRepsoitory.java
+++ todo/src/main/java/com/aivid/todo/repository/TaskRepsoitory.java
@@ -1,4 +1,15 @@
 package com.aivid.todo.repository;
 
+import com.aivid.todo.model.Task;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface TaskRepsoitory {
+
+    List<Task> getAllTasks();
+    void save(Task task);
+    Task findTaskByUUID(UUID uuid);
+    void update (Task task);
+    void delete (Task task);
 }
