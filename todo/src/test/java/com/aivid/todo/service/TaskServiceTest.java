3a9e638d047f287ecbc03612b0d26f28b9ceda08
Adrian Debowski <adrian.debowski@outlook.com>
2023-06-24 21:44:07 +0200

    Created test for methods of All Tasks.

--- todo/src/test/java/com/aivid/todo/service/TaskServiceTest.java
+++ todo/src/test/java/com/aivid/todo/service/TaskServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,23 +16,52 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class TaskServiceTest {
 
+    private final TaskService taskService = new TaskService();
+
     private final LocalDate dateToday = LocalDate.now();
     private final LocalDate dateTomorrow = LocalDate.now().plusDays(1);
+    private final LocalDate datePlusTen = LocalDate.now().plusDays(10);
     private final LocalDate dateNextMonth = LocalDate.now().plusDays(31);
 
     private final Task taskDueTomorrow = new Task(UUID.randomUUID(),"Test task Tomorrow", "Test task due tomorrow", Category.HOME, Priority.URGENT, dateToday,dateTomorrow);
+    private final Task taskDueTenDays = new Task(UUID.randomUUID(),"Test task ten", "Test task due within ten days", Category.WORK, Priority.HIGH, dateToday,datePlusTen);
+    private final Task taskDueMonth = new Task(UUID.randomUUID(),"Test task month", "Test task due within month", Category.HOBBY, Priority.LOW, dateToday,dateNextMonth);
 
     @Test
     @DisplayName("Test which verify if task is added.")
     void addTask() {
         // Given
-        TaskService taskService = new TaskService();
-        // When
         taskService.addTask(taskDueTomorrow);
+        // When
         Task retrieveTasks = taskService.getTaskById(taskDueTomorrow.getId());
         // Then
         assertNotNull(retrieveTasks);
         assertEquals(taskDueTomorrow, retrieveTasks);
+
+    }
+
+    @Test
+    void getAllTasks() {
+        // Given
+        taskService.addTask(taskDueTomorrow);
+        taskService.addTask(taskDueTenDays);
+        taskService.addTask(taskDueMonth);
+
+        List<Task> testListOfTasks = List.of(taskDueTomorrow,taskDueTenDays,taskDueMonth);
+        // When
+        List<Task> retrieveAllTasks = taskService.getAllTasks();
+
+        // Then
+        assertNotNull(retrieveAllTasks);
+        assertEquals(testListOfTasks,retrieveAllTasks);
+
+    }
+
+    @Test
+    void getTaskById() {
+        // When
+
+        // Then
 
     }
 }
